604bf36fe52fc90737b38ecbc3164c4fd566da8a
Andrew Glowik <amglowik@verizon.net>
2015-01-04 19:14:58 -0500

    Initial commit

---
+++ EnumSample/src/main/java/com/glowik/Readable.java
@@ -0,0 +1,8 @@
+package com.glowik;
+
+/**
+ * Created by amglowik on 11/28/2014.
+ */
+public interface Readable {
+    NutritionFacts read();
+}
---
+++ EnumSample/src/main/java/com/glowik/Writable.java
@@ -0,0 +1,8 @@
+package com.glowik;
+
+/**
+ * Created by amglowik on 11/28/2014.
+ */
+public interface Writable {
+   void write(NutritionFacts value);
+}
